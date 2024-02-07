9d0fd3eaeb743fd4e2895d4f2bb1ba4cd696dc83
Sunyoung Seol <141697063+sunnyseol@users.noreply.github.com>
2024-02-07 20:17:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const todoArray = [{
+  content: '선물 주문하기',
+  date: '2024-01-30'
+},{
+  content: '주유하기',
+  date:'2024-01-30'
+}];
+
+/*
+1. input value 가져오기
+2. DOM 화면에 표기하기
+3. 지우기 버튼 만들기 */
+displayTodo();
+
+function displayTodo() {
+  let todoHtml = '';
+
+  todoArray.forEach((object, index) => {
+    const { content, date } = object;
+    const html = `
+    <div>${content}</div>
+    <div>${date}</div>
+    <button class="delete-button">지우기</button>`;
+
+    todoHtml += html;
+  });
+
+  document.querySelector('.js-display').innerHTML = todoHtml;
+
+
+  document.querySelectorAll('.delete-button').forEach((deletButtonObj, i) => {
+    deletButtonObj.addEventListener('click', () => {
+      todoArray.splice(i, 1);
+      displayTodo();
+    });
+  });
+}
+
+document.querySelector('.js-add-button').addEventListener('click', () => { inputElement();
+});
+
+
+document.body.addEventListener('keydown', (event) => {
+  if(event.key === 'Enter') {
+    inputElement();
+  }
+});
+
+
+function inputElement() {
+
+  const inputContent = document.querySelector('.js-input-content');
+  const inputContentValue = inputContent.value;
+
+  const inputDate = document.querySelector('.js-input-date');
+  const inputDateValue = inputDate.value;
+
+  if(inputContentValue === '') {
+    return;
+  }
+
+  todoArray.push({
+    content : inputContentValue,
+    date : inputDateValue
+  });
+
+  inputContent.value = '';
+
+  displayTodo();
+}
+
+
+
+
+
+
+
